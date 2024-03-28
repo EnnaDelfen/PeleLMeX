db7522dc722e81895d5ec466bd34a8e0000e4f47
EnnaDelfen <fanne.af@gmail.com>
2024-03-28 15:50:32 +0100

    Add FlameSheet to the suite.

---
+++ Exec/RegTests/EB_Flamesheet/pelelmex_prob.cpp
@@ -0,0 +1,19 @@
+#include <PeleLMeX.H>
+#include <AMReX_ParmParse.H>
+
+void
+PeleLM::readProbParm() // NOLINT(readability-make-member-function-const)
+{
+  amrex::ParmParse pp("prob");
+
+  pp.query("P_mean", prob_parm->P_mean);
+  pp.query("T_input", prob_parm->T_input);
+  //pp.query("Y_fuel", prob_parm->Y_fuel);
+  //pp.query("Y_oxid", prob_parm->Y_o2);
+  //pp.query("T_hot", prob_parm->T_hot);
+  //pp.query("T_wall", prob_parm->Twall);
+  pp.query("meanFlowMag", prob_parm->meanFlowMag);
+  pp.query("EBinflow_vel", prob_parm->EBinflow_vel);
+  pp.query("EBinflow_T", prob_parm->EBinflow_T);
+  pp.query("EB_isoTwall", prob_parm->EB_isoTwall);
+}
