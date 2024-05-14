b4c107ae198cbdd5f2e956222b198cb6231a19ea
EnnaDelfen <fanne.af@gmail.com>
2024-05-14 22:01:03 +0200

    Make reactive or non reactive init an input choice + specify T of hot gases for ignition

--- Exec/RegTests/EB_Flamesheet/pelelmex_prob.cpp
+++ Exec/RegTests/EB_Flamesheet/pelelmex_prob.cpp
@@ -8,9 +8,10 @@ PeleLM::readProbParm() // NOLINT(readability-make-member-function-const)
 
   pp.query("P_mean", prob_parm->P_mean);
   pp.query("T_input", prob_parm->T_input);
+  pp.query("is_reactive", prob_parm->is_reactive);
   //pp.query("Y_fuel", prob_parm->Y_fuel);
   //pp.query("Y_oxid", prob_parm->Y_o2);
-  //pp.query("T_hot", prob_parm->T_hot);
+  pp.query("T_hot", prob_parm->T_hot);
   //pp.query("T_wall", prob_parm->Twall);
   pp.query("meanFlowMag", prob_parm->meanFlowMag);
   pp.query("EBinflow_vel", prob_parm->EBinflow_vel);
